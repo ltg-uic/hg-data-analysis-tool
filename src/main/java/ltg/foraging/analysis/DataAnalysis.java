d8f0df65d7a90544ef6deef4e8e9ae59a9aa499d
Alessandro Gnoli <tebemis@gmail.com>
2013-04-16 18:32:23 +0200

    First take @ CSV export

--- src/main/java/ltg/foraging/analysis/DataAnalysis.java
+++ src/main/java/ltg/foraging/analysis/DataAnalysis.java
@@ -3,65 +3,111 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.TimeZone;
 
-import com.github.jsonj.JsonArray;
-import com.github.jsonj.JsonElement;
-import com.github.jsonj.JsonObject;
-import com.github.jsonj.exceptions.JsonParseException;
-import com.github.jsonj.tools.JsonParser;
+import au.com.bytecode.opencsv.CSVWriter;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class DataAnalysis {
-	
-	private int gameBeginTime = -1;
-	private int gameEndTime = -1;
-	private Histories h = new Histories();
-	
-	
+
+	private List<ObjectNode> jsonData = new ArrayList<ObjectNode>();
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		DataAnalysis da = new DataAnalysis();
 		da.importData();
-		da.doAnalysis();
-		da.printResults();
+		da.dumpToCSV();
+		//da.doAnalysis();
+		//da.printResults();
+	}
+
+
+	public void dumpToCSV() {
+		// Init CSV file
+		CSVWriter writer = null;
+		try {
+			writer = new CSVWriter(new FileWriter("data/helio_sp_13/ben_log.csv"));
+			//writer = new CSVWriter(new FileWriter("data/helio_sp_13/julia_log.csv"));
+			Iterator<ObjectNode> i = jsonData.iterator();
+			while (i.hasNext()) {
+				ObjectNode n = i.next();
+				if (n.get("event").asText().equals("init_helio") || 
+						n.get("event").asText().equals("init_helio_diff")) {
+					//i.remove();
+				} else {
+					writer.writeNext(entryToArray(n));
+				}
+			}
+			writer.close();
+		} catch (IOException e) {
+			System.out.println("Can't create the CSV file, terminating");
+			System.exit(-1);
+		}
+	}
+
+
+	private String[] entryToArray(ObjectNode n) {
+		String[] entries = new String[6];	
+		Date d = new Date(1000*getTs(n));
+		SimpleDateFormat df = new SimpleDateFormat("mm/dd/yy HH:mm:ss");
+		df.setTimeZone(TimeZone.getTimeZone("America/Toronto"));
+		entries[0] = df.format(d);
+		Long.toString(getTs(n));
+		entries[1] = removeNull(n.get("origin"));
+		entries[2] = removeNull(n.get("event"));
+		entries[3] = removeNull(n.get("payload").get("anchor"));
+		entries[4] = removeNull(n.get("payload").get("color"));
+		entries[5] = removeNull(n.get("payload").get("reason"));
+		return entries;
 	}
 	
+	private String removeNull(JsonNode jsonNode) {
+		if (jsonNode!=null)
+			return jsonNode.asText();
+		else
+			return "";
+	}
+
 
 	public void importData() {
-		JsonParser parser = new JsonParser();
-		List<JsonObject> jsonData = new ArrayList<JsonObject>();
+		ObjectMapper jsonParser = new ObjectMapper();
+
 		// Read from file into jsonData list of json objects
 		FileInputStream fstream = null;
 		try {
-//			fstream = new FileInputStream("data/pilot_1/foraging_pilot_oct12_1_log_1.json");
-//			fstream = new FileInputStream("data/pilot_1/foraging_pilot_oct12_1_log_2.json");
-//			fstream = new FileInputStream("data/pilot_1/foraging_pilot_oct12_1_log_3.json");
-//			fstream = new FileInputStream("data/pilot_1/foraging_pilot_oct12_2_log_1.json");
-//			fstream = new FileInputStream("data/pilot_1/foraging_pilot_oct12_2_log_2.json");
-			fstream = new FileInputStream("data/pilot_1/foraging_pilot_oct12_2_log_3.json");		
+			fstream = new FileInputStream("data/helio_sp_13/ben_log.json");
+			//			fstream = new FileInputStream("data/helio_sp_13/julia_log.json");		
 			DataInputStream in = new DataInputStream(fstream);
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
 			String strLine = null;
-			JsonObject json = null;
-			JsonElement jsone = null;
+			JsonNode jn = null;
 			try {
 				while ((strLine = br.readLine()) != null)   {
 					try {
-						jsone = parser.parse(strLine);
-						if (!jsone.isObject()) {
+						jn = jsonParser.readTree(strLine);
+						if (!jn.isObject()) {
 							System.err.println("This is not a JSON object!!!");
+							System.exit(-1);
 						}
-						json = jsone.asObject();
-						jsonData.add(json);
+						jsonData.add((ObjectNode) jn);
 					} catch (JsonParseException e) {
+						System.err.println("Error parsing: this is not a JSON object!!!");
+						System.exit(-1);
 					}
-					
 				}
 			} catch (IOException e) {
 				System.err.println("Impossible to parse file line");
@@ -73,72 +119,31 @@ public class DataAnalysis {
 			System.err.println("Impossible to close file");
 		}
 		// Check sequence is ordered by timestamp
-		int lastTs = -1;
-		for (JsonObject o: jsonData) {
-			int currentTs = getTs(o);
+		long lastTs = -1;
+		for (ObjectNode o: jsonData) {
+			long currentTs = getTs(o);
 			if (lastTs <= currentTs)
 				lastTs = currentTs;
 			else
 				System.err.println("Sequence is not ordered properly... BAD!");
 		}
-		// Parse JSON objects into individual histories
-		for (JsonObject o: jsonData) {
-			if (o.getString("event").equals("game_reset")) {
-				gameBeginTime =  getTs(o);
-				continue;
-			}
-			if (o.getString("event").equals("game_stop")) {
-				gameEndTime = getTs(o);
-				continue;
-			}
-			if (o.getString("event").equals("rfid_update")) {
-				String dest = o.getString("destination");
-				JsonArray a = o.getArray("payload", "arrivals");
-				JsonArray d = o.getArray("payload", "departures");
-				if (a.isEmpty())
-					h.addAction(d.get(0).toString(), getTs(o), -1, dest);  	// It's a departure
-				else
-					h.addAction(a.get(0).toString(), getTs(o),  1, dest); 	// It's an arrival
-				continue;
-			}
-			// Parse kills events
-			if (o.getString("event").equals("kill_bunny")) {
-				String dest = o.getString("destination");
-				String id = o.getString("payload", "id");
-				h.addKill(id, dest);
-				continue;
-			}
-			//System.err.println("Unknown message type... what!");
-		}
-		// Validate all action sequences
-		h.validateActionSequences();
 	}
-	
-	
-	
+
+
+
 	public void doAnalysis() {
-		// Compute amount of time spent by every kid at every patch 
-		h.computePatchTimes(gameEndTime);
-		// Total time spent at the six patches and den
-		h.computeTotalPatchTimes();
-		// Compute amount of food gathered by every kid at every patch 
-		h.computeHarvest(gameBeginTime, gameEndTime);
-		// Total time there were 0, 1, 2,...n kids at the patch
-		h.computeKidsAtPatch();
-		// Compute the quality of patch switches
-		h.computeQualityOfPatchSwitches(gameBeginTime, gameEndTime);
+
 	}
-	
-	
+
+
 	public void printResults() {
-		System.out.println("Total game run time: "+(gameEndTime-gameBeginTime)+"s\n");
-		h.printResults();
+
 	}
 
-	
 
-	private int getTs(JsonObject o) {
-		return Integer.parseInt(o.getString("_id", "$oid").substring(0, 8), 16);
+
+	private long getTs(ObjectNode o) {
+		return Integer.parseInt(o.get("_id").get("$oid").asText().substring(0, 8), 16);
 	}
 
 }
