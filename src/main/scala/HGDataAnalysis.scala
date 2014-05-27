0a643f4bb9785e3476b15e0fe59fad237ae4e894
Alessandro Gnoli <tebemis@gmail.com>
2014-05-26 21:09:14 -0500

    Added data, created app and stubbed basic docs

---
+++ src/main/scala/HGDataAnalysis.scala
@@ -0,0 +1,13 @@
+/**
+ * Created by tebemis on 5/26/14.
+ */
+
+object HGDataAnalysis extends App {
+  print("Hunger Games Data Analysis script!\nStarting...\n")
+
+  // Read data
+  // Clean data
+  // Perform analysis
+  // Dump outputs
+
+}
